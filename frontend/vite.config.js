85caa92411a85fe21281a144260817675766f4af
Aryan Gusain <aryangusain123@gmail.com>
2024-09-11 09:02:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://rest-api-snowy.vercel.app/'
+      '/api': 'https://rest-api-snowy.vercel.app'
     }
   },
   plugins: [react()],
